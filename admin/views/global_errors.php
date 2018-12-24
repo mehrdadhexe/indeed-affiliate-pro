03711462ade22c7059566e8d863f284b5bff1eb7
mehrdadhexe <mehrdad.h.exe@gmail.com>
2018-12-24 21:12:26 +0330

    V 1.0.0

---
+++ admin/views/global_errors.php
@@ -0,0 +1,3 @@
+<div class="<?php echo $data['err_class'];?>"><?php  
+echo __('<p>This is a Trial Version of <strong>همکاری در فروش</strong> plugin. Please add your purchase code into Licence section to enable the Full همکاری در فروش Version. Check your ', 'uap') . '<a href="' . $data['url'] . '">' . __('licence section', 'uap') .'</a>.</p>';
+?></div>
